e43fc8494cdf6ae13fe11518519bed7ae1d3a214
Martonvok <martonsom@gmail.com>
2023-09-23 16:20:17 +0200

    OPP kész

--- src/kartyatrukkkonzolon/Kartyatrukkoop/Kartyatrukk.java
+++ src/kartyatrukkkonzolon/Kartyatrukkoop/Kartyatrukk.java
@@ -1,29 +1,34 @@
-package kartyatrukkkonzolon;
+package kartyatrukkkonzolon.Kartyatrukkoop;
 
 import java.util.Scanner;
 
-
 public class Kartyatrukk {
-    private Scanner sc = new Scanner(System.in);
+
+    private static final Scanner sc = new Scanner(System.in);
+    private Pakli pakli;
 
     public Kartyatrukk() {
-        Pakli p = new Pakli();
-        int oszlop = 2;
-        melyik(oszlop);
+        int oszlop = 0;
+        this.pakli = new Pakli();
+        for (int i = 0; i < 3; i++) {
+            this.pakli.kirak();
+            melyik(oszlop);
+        }
+        this.pakli.ezVolt();
     }
 
-    private int melyik(int oszlop) {
+    public static void main(String[] args) {
+        new Kartyatrukk();
+    }
+
+    private void melyik(int oszlop) {
         boolean jo;
         do {
             System.out.print("melyik oszlop (1-3): ");
             oszlop = sc.nextInt();
             jo = oszlop >= 1 && oszlop <= 3;
         } while (!jo);
-        return oszlop;
+        this.pakli.kever(oszlop);
     }
-    
 
-    
-
-   
 }
--- src/kartyatrukkkonzolon/Kartyatrukkoop/Pakli.java
+++ src/kartyatrukkkonzolon/Kartyatrukkoop/Pakli.java
@@ -1,15 +1,13 @@
-package kartyatrukkkonzolon;
+package kartyatrukkkonzolon.Kartyatrukkoop;
+
+import kartyatrukkkonzolon.Kartyatrukkoop.Lap;
 
 public class Pakli {
 
-    private String[] pakli = new String[22];
+    private static Lap[] pakli = new Lap[22];
 
     public Pakli() {
         feltolt();
-        kirak();
-        kever();
-        ezVolt();
-
     }
 
     private void feltolt() {
@@ -18,34 +16,49 @@ public class Pakli {
         int i = 1;
         for (String szin : szinek) {
             for (int e = 0; e < ertekek.length && i < pakli.length; e++) {
-                pakli[i++] = szin + "_" + ertekek[e];
+                pakli[i++] = new Lap(szin + "_" + ertekek[e]);
             }
         }
     }
 
-    private void kirak() {
+    void kirak() {
         for (int i = 1; i < pakli.length; i++) {
-            System.out.printf("%-8s", pakli[i]);
+            System.out.printf("%-8s", pakli[i].getLeiras());
             if (i % 3 == 0) {
                 System.out.println("");
             }
         }
     }
 
-    private void kever() {
-        //megkeveri a lapokat
+    void kever(int oszlop) {
+        Lap[] ujPakli = new Lap[22];
+        switch (oszlop) {
+            case 1:
+                for (int i = 1; i <= 7; i++) {
+                    ujPakli[i] = pakli[20 - (i - 1) * 3];
+                    ujPakli[i + 7] = pakli[19 - (i - 1) * 3];
+                    ujPakli[i + 14] = pakli[21 - (i - 1) * 3];
+                }
+                break;
+            case 2:
+                for (int i = 1; i <= 7; i++) {
+                    ujPakli[i] = pakli[19 - (i - 1) * 3];
+                    ujPakli[i + 7] = pakli[20 - (i - 1) * 3];
+                    ujPakli[i + 14] = pakli[21 - (i - 1) * 3];
+                }
+                break;
+            case 3:
+                for (int i = 1; i <= 7; i++) {
+                    ujPakli[i] = pakli[19 - (i - 1) * 3];
+                    ujPakli[i + 7] = pakli[21 - (i - 1) * 3];
+                    ujPakli[i + 14] = pakli[20 - (i - 1) * 3];
+                }
+                break;
+        }
+        pakli = ujPakli;
     }
 
-    private void ezVolt() {
-        System.out.println("A választott lap: " + pakli[11]);
+    void ezVolt() {
+        System.out.println("A választott lap: " + pakli[11].getLeiras());
     }
-
-    public String[] getPakli() {
-        return pakli;
-    }
-
-    public void setPakli(String[] pakli) {
-        this.pakli = pakli;
-    }
-
 }
